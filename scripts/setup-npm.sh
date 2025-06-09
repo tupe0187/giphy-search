1886b360598c810bd41f3980a1b14dc396cbcf65
Ketan <tupe0187@gmail.com>
2025-06-09 21:38:25 +1000

    Bug fix setup-npm.sh config keyword issue

--- scripts/setup-npm.sh
+++ scripts/setup-npm.sh
@@ -6,8 +6,7 @@ set -eu -o pipefail
 echo "Begin: setup npm"
 
 export NODE_OPTIONS=--max-old-space-size=6144
-npm config set unsafe-perm true
 
-npm install -g npm@7
+npm install -g npm@7 --unsafe-perm
 
 echo "Finish: setup npm"
